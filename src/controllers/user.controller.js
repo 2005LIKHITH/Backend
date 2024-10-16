e9135456b080b50f82a80e8ad12b954f4929fbd7
2005LIKHITH <likhithcvsrl@gmail.com>
2024-10-16 17:28:32 +0530

    commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,8 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 import ApiError from "../utils/ApiError.js";
 import {User} from "../model/user.model.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import {ApiResponse} from "../utils/ApiResponse.js";
 const registerUser = asyncHandler(async (req, res) => {
     // Here, you might want to add your registration logic
     // res.status(200).json({ message: "REGISTERED USER !!" });
@@ -23,6 +25,33 @@ const registerUser = asyncHandler(async (req, res) => {
     const coverImageLocalPath = req.files?.cover[0]?.path;
 
     if(!avatarLocalPath)throw new ApiError(400,"Avatar is required");
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage  = await uploadOnCloudinary(coverImageLocalPath);
+
+    if(!avatar)throw new ApiError(400,"Avatar upload failed");
+
+    const user = User.create({
+        fullName,
+        avatar : avatar.url,
+        coverImage : coverImage?.url || "",
+        email,
+        password,
+        username : username.toLowerCase(),
+
+
+    })
+
+    const createdUser = await user.findbyObjectId(user._id).select(
+        "-password -refreshToken" //we don't want to send password and refreshToken
+    );
+
+    if(!createdUser)throw new ApiError(500,"Something went wrong while registering the user");
+
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"User Registered Successfully !!")
+    )
+
 
 }); 
 
