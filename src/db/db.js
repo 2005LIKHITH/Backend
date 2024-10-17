327ba9601ea0614a888feb262add76f71bbde823
2005LIKHITH <likhithcvsrl@gmail.com>
2024-10-17 21:54:50 +0530

    Code Quality Imporvement in db.js

--- src/db/db.js
+++ src/db/db.js
@@ -11,13 +11,19 @@ const connectDB = async () => {
 // console.log("DB_NAME:", DB_NAME);
 // console.log("Final Connection URI:", `${process.env.MONGODB_URI}/${DB_NAME}`);
 
+        // mongoose.connect will return us an instance of the connection 
+
+        //DB_NAME naamak database banayega Iss provided URL Me 
+        // console.log(process.env.MONGODB_URI)
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`); 
         console.log(`\nMongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+        //so what we are doing using we are creating a database named DB_NAME in the host
+        const db = mongoose.connection;
 
-        mongoose.connection.on('disconnected', () => {
+        db.on('disconnected', () => {
             console.log("\nMongoDB disconnected");
         });
-        mongoose.connection.on('reconnected', () => {
+        db.on('reconnected', () => {
             console.log("\nMongoDB reconnected");
         });
     } catch (err) {
